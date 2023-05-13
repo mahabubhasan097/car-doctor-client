2bea254eb5e94e39559cd4fc4b4da12c600303f7
mahabubhasan097 <mhrafi1026@gmail.com>
2023-05-13 23:13:29 +0600

    first commit

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import img1 from '../../../assets/images/banner/1.jpg'
+import img2 from '../../../assets/images/banner/2.jpg'
+import img3 from '../../../assets/images/banner/3.jpg'
+import img4 from '../../../assets/images/banner/4.jpg'
+import img5 from '../../../assets/images/banner/5.jpg'
+import img6 from '../../../assets/images/banner/6.jpg'
+const Banner = () => {
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full h-[700px] bg-gradient-to-r from-black to-red-100 rounded-xl">
+                <img src={img1} className="w-full mix-blend-overlay rounded-xl" />
+                <div className="absolute flex justify-start align-middle transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className='w-1/2'>
+                        <h2 className='text-6xl text-white font-extrabold'>Affordable price for car servicing</h2>
+                        <p className='my-9 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nesciunt incidunt esse aut illum facere hic itaque tempore repellendus quasi?</p>
+                        <button className="btn btn-primary mr-4">Discove More</button>
+                        <button className="btn btn-outline btn-secondary">Latest Project</button>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide6" className="btn btn-circle bg-indigo-500 mr-4">❮</a>
+                    <a href="#slide2" className="btn btn-circle bg-indigo-500">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full h-[700px] bg-gradient-to-r from-black to-red-100 rounded-xl">
+                <img src={img2} className="w-full mix-blend-overlay rounded-xl" />
+                <div className="absolute flex justify-start align-middle transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className='w-1/2'>
+                        <h2 className='text-6xl text-white font-extrabold'>Affordable price for car servicing</h2>
+                        <p className='my-9 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nesciunt incidunt esse aut illum facere hic itaque tempore repellendus quasi?</p>
+                        <button className="btn btn-primary mr-4">Discove More</button>
+                        <button className="btn btn-outline btn-secondary">Latest Project</button>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle bg-indigo-500 mr-4">❮</a>
+                    <a href="#slide3" className="btn btn-circle bg-indigo-500">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full h-[700px] bg-gradient-to-r from-black to-red-100 rounded-xl">
+                <img src={img3} className="w-full mix-blend-overlay rounded-xl" />
+                <div className="absolute flex justify-start align-middle transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className='w-1/2'>
+                        <h2 className='text-6xl text-white font-extrabold'>Affordable price for car servicing</h2>
+                        <p className='my-9 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nesciunt incidunt esse aut illum facere hic itaque tempore repellendus quasi?</p>
+                        <button className="btn btn-primary mr-4">Discove More</button>
+                        <button className="btn btn-outline btn-secondary">Latest Project</button>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle bg-indigo-500 mr-4">❮</a>
+                    <a href="#slide4" className="btn btn-circle bg-indigo-500">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full h-[700px] bg-gradient-to-r from-black to-red-100 rounded-xl">
+                <img src={img4} className="w-full mix-blend-overlay rounded-xl" />
+                <div className="absolute flex justify-start align-middle transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className='w-1/2'>
+                        <h2 className='text-6xl text-white font-extrabold'>Affordable price for car servicing</h2>
+                        <p className='my-9 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nesciunt incidunt esse aut illum facere hic itaque tempore repellendus quasi?</p>
+                        <button className="btn btn-primary mr-4">Discove More</button>
+                        <button className="btn btn-outline btn-secondary">Latest Project</button>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide3" className="btn btn-circle bg-indigo-500 mr-4">❮</a>
+                    <a href="#slide5" className="btn btn-circle bg-indigo-500">❯</a>
+                </div>
+            </div>
+            <div id="slide5" className="carousel-item relative w-full h-[700px] bg-gradient-to-r from-black to-red-100 rounded-xl">
+                <img src={img5} className="w-full mix-blend-overlay rounded-xl" />
+                <div className="absolute flex justify-start align-middle transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className='w-1/2'>
+                        <h2 className='text-6xl text-white font-extrabold'>Affordable price for car servicing</h2>
+                        <p className='my-9 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nesciunt incidunt esse aut illum facere hic itaque tempore repellendus quasi?</p>
+                        <button className="btn btn-primary mr-4">Discove More</button>
+                        <button className="btn btn-outline btn-secondary">Latest Project</button>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle bg-indigo-500 mr-4">❮</a>
+                    <a href="#slide6" className="btn btn-circle bg-indigo-500">❯</a>
+                </div>
+            </div>
+            <div id="slide6" className="carousel-item relative w-full h-[700px] bg-gradient-to-r from-black to-red-100 rounded-xl">
+                <img src={img6} className="w-full mix-blend-overlay rounded-xl" />
+                <div className="absolute flex justify-start align-middle transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className='w-1/2'>
+                        <h2 className='text-6xl text-white font-extrabold'>Affordable price for car servicing</h2>
+                        <p className='my-9 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, nesciunt incidunt esse aut illum facere hic itaque tempore repellendus quasi?</p>
+                        <button className="btn btn-primary mr-4">Discove More</button>
+                        <button className="btn btn-outline btn-secondary">Latest Project</button>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide5" className="btn btn-circle bg-indigo-500 mr-4">❮</a>
+                    <a href="#slide1" className="btn btn-circle bg-indigo-500">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
